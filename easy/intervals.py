df92028dcd3db45a08117d504521ea1f4feccd48
Kirill <andrewthechf@gmail.com>
2025-05-04 13:55:07 +0300

    Non intersection intervals

--- easy/intervals.py
+++ easy/intervals.py
@@ -19,15 +19,56 @@ def non_intersection_intervals(intervals: list[tuple]) -> list[tuple]:
 
     return non_intersect_intervals
 
+# if __name__ == "__main__":
+#     many_intervals = [
+#         [(1, 4), (3, 6), (5, 8), (7, 10)],
+#         [(1, 10), (2, 5), (6, 8), (12, 15)],
+#         [(1, 2), (4, 5), (7, 9)],
+#     ]
+#
+#     for interval in many_intervals:
+#         result = non_intersection_intervals(interval)
+#         print(result)
+
+
+def extended_intervals(intervals: list[tuple]) -> list[tuple]:
+    start_pair = intervals[0]
+    result = [start_pair]
+
+    is_refreshed = False
+    left_position = 0
+    right_position = 1
+
+    while right_position <= len(intervals) - 1:
+        if not is_refreshed:
+            left_pair = list(intervals[left_position])
+
+        right_pair = list(intervals[right_position])
+        if left_pair[1] < right_pair[0]:
+            result.append(tuple(right_pair))
+            left_position = right_position
+            is_refreshed = False
+
+        if left_pair[1] > right_pair[0] and left_pair[1] < right_pair[1]:
+            left_pair[1] = right_pair[1]
+            result[left_position] = tuple(left_pair)
+
+            is_refreshed = True
+
+        right_position += 1
+
+    return result
+
 if __name__ == "__main__":
     many_intervals = [
         [(1, 4), (3, 6), (5, 8), (7, 10)],
         [(1, 10), (2, 5), (6, 8), (12, 15)],
         [(1, 2), (4, 5), (7, 9)],
+        [(1, 5), (3, 7), (7, 9)],
     ]
 
     for interval in many_intervals:
-        result = non_intersection_intervals(interval)
+        result = extended_intervals(interval)
         print(result)
 
 
--- easy/merge_sort.py
+++ easy/merge_sort.py
@@ -29,4 +29,20 @@ list2 = [2,54,765,2,4,8,0,87,56,34]
 list3 = list1 + list2
 result = merge_sort(list3)
 print(result)
-a = 0
+
+
+def merge_sort2(arr1, arr2) -> list:
+    result = []
+    while arr1 and arr2:
+        if arr1[0] < arr2[0]:
+            result.append(arr1.pop(0))
+        else:
+            result.append(arr2.pop(0))
+
+    result.extend(arr1)
+    result.extend(arr2)
+    return result
+
+
+res = merge_sort2([0,3,4,31], [4,6,30])
+print(res, 'from course')
